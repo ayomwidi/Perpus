a4c314b986f466b7299d470ef20d2d32b2b67499
ayomwidi <ayom_widi@yahoo.com>
2019-08-13 11:47:04 +0700

    first commit

---
+++ application/config/database.php
@@ -0,0 +1,24 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$active_group = 'default';
+$active_record = TRUE;
+
+$db['default']['hostname'] = 'myrds.cuh06brc83fm.us-east-1.rds.amazonaws.com';
+$db['default']['username'] = 'admin';
+$db['default']['password'] = 'legend123';
+$db['default']['database'] = 'db_perpus';
+$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
+
+
+/* End of file database.php */
+/* Location: ./application/config/database.php */
